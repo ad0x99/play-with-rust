8f814bfc8c125699c348669f1e56bb31b1cd4b1d
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-01-23 16:34:39 +0700

    feat(the-rust): restructure restaurant examples

--- the-rust-programming-language/restaurant/src/main.rs
+++ the-rust-programming-language/restaurant/src/main.rs
@@ -1,4 +1,21 @@
-use restaurant::eat_at_restaurant;
+use restaurant;
+
 fn main() {
-    eat_at_restaurant();
+    println!("=====eat_at_restaurant=====");
+    restaurant::eat_at_restaurant();
+
+    println!("=====print_shortcut_path=====");
+    restaurant::print_shortcut_path();
+
+    println!("=====print_using_path_with_use_key_word=====");
+    restaurant::print_using_path_with_use_key_word();
+
+    println!("=====print_relative_path_with_super=====");
+    restaurant::print_relative_path_with_super();
+
+    println!("=====print_using_pub_with_struct=====");
+    restaurant::print_using_pub_with_struct();
+
+    println!("=====print_using_pub_with_enums=====");
+    restaurant::print_using_pub_with_enums();
 }
