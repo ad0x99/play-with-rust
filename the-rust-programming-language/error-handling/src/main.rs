bee0ba1d80868dbe91d9c4898926e0263007b627
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-01-31 15:42:13 +0700

    feat(the-rust): error handling in rust

---
+++ the-rust-programming-language/error-handling/src/main.rs
@@ -0,0 +1,14 @@
+mod unrecoverable_error;
+mod recoverable_error;
+mod propagating_error;
+
+fn main() {
+    println!("========Unrecoverable Errors========");
+    // unrecoverable_error::run_unrecoverable_error();
+
+    println!("========Recoverable Errors========");
+    // recoverable_error::run_unrecoverable_error();
+
+    println!("========Propagating Errors========");
+    propagating_error::run_propagating_error();
+}
---
+++ the-rust-programming-language/error-handling/src/unrecoverable_error.rs
@@ -0,0 +1,7 @@
+pub fn run_unrecoverable_error() {
+    crash_and_burn();
+}
+
+fn crash_and_burn() {
+    panic!("Crash and burn!")
+}
