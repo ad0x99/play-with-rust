42a3d570337e404eb22224d70a7b2a643b7b94c2
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-03-15 17:05:25 +0700

    feat(the-rust): add smart pointer - reference with deref and deref coercions

---
+++ the-rust-programming-language/smart_pointer/reference-with-deref/src/cleanup_with_drop_trait.rs
@@ -0,0 +1,29 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+pub fn cleanup_with_drop_trait() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+}
+
+pub fn cleanup_manually_with_drop_fn() {
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
