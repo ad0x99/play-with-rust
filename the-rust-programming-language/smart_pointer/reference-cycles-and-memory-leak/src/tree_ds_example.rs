578202bf7ea7fb05bf9500ba3703a4970a84ea28
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-04-10 22:21:47 +0700

    feat(the-rust): add smart pointer - reference cycles and memory leak

---
+++ the-rust-programming-language/smart_pointer/reference-cycles-and-memory-leak/src/tree_ds_example.rs
@@ -0,0 +1,88 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+pub fn run_tree() {
+    run_tree_with_weak_reference();
+    run_tree_visualize_strong_and_weak_counts();
+}
+// fn run_tree_1() {
+//     let leaf = Rc::new(Node {
+//         value: 3,
+//         children: RefCell::new(vec![]),
+//     });
+
+//     let branch = Rc::new(Node {
+//         value: 5,
+//         children: RefCell::new(vec![Rc::clone(&leaf)]),
+//     });
+// }
+
+fn run_tree_with_weak_reference() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+}
+
+fn run_tree_visualize_strong_and_weak_counts() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
