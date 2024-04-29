9a7154069032620f6e8b47080cde254c72f379a0
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-04-29 22:29:14 +0700

    feat(the-rust): pattern and matching - extra conditionals and match guards

--- the-rust-programming-language/pattern_and_matching/src/main.rs
+++ the-rust-programming-language/pattern_and_matching/src/main.rs
@@ -1,9 +1,18 @@
 mod destructuring;
+mod extra_conditionals_and_match_guards;
 mod ignore_values_in_pattern;
 mod pattern_syntax;
 
 fn main() {
+    println!("============Pattern Syntax============");
     pattern_syntax::run_pattern_syntax();
+
+    println!("============Pattern Destructuring============");
     destructuring::run_destructuring();
+
+    println!("============Ignore Values in Pattern============");
     ignore_values_in_pattern::run_ignore_values();
+
+    println!("============Extra conditionals & Match guards============");
+    extra_conditionals_and_match_guards::run_extra_conditionals_and_match_guards();
 }
